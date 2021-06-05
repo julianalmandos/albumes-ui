32a278fe95c8b372eedc1afad8549d61aacc721c
Almandos, Julian Luis <julianalmandos@gmail.com>
2021-06-05 15:04:16 -0300

    Install vue-youtube-embed dependency

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueYouTubeEmbed from 'vue-youtube-embed'
 
 require('./assets/reset.css');
 require('./assets/root.css');
 
 Vue.config.productionTip = false
+Vue.use(VueYouTubeEmbed)
 
 new Vue({
   router,
