df156e172f5cf04d8fa194f1674d397c068b44ae
Andrés Milla <andressmilla@gmail.com>
2021-06-22 15:26:03 -0300

    Added hover in navbar links

--- src/components/NavbarLink.vue
+++ src/components/NavbarLink.vue
@@ -28,6 +28,15 @@ export default {
   color: inherit;
 }
 
+.anchor:hover {
+  color: #e6f8ff;
+  transition: color 0.2s ease-in-out;
+  -moz-transition: color 0.2s ease-in-out;
+  -webkit-transition: color 0.2s ease-in-out;
+  -o-transition: color 0.2s ease-in-out;
+  -ms-transition: color 0.2s ease-in-out;
+}
+
 .anchor > * {
   margin-bottom: 0.5rem;
 }
