5dbd1747d59c12498951ab6101742acdb34f3745
Almandos, Julian Luis <julianalmandos@gmail.com>
2021-07-14 23:46:43 -0300

    Fix text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h2 class="home__title">¡Bienvenido a Álbumes!</h2>
-    <h3 class="home__subtitle">Creá, elegí e imprimí tus propios álbumes para concientizar a la población sobre desastres naturales.</h3>
+    <h3 class="home__subtitle">Crea, elige e imprime tus propios álbumes para concientizar a la población sobre desastres naturales.</h3>
     <ol class="home__steps">
       <li class="home__step">
         <FirstStep />        
@@ -9,15 +9,15 @@
       </li>
       <li class="home__step">
         <SecondStep />
-        <p>Elegí entre las más de 10 entrevistas a sobrevivientes de desastres naturales.</p>
+        <p>Elige entre las más de 10 entrevistas a sobrevivientes de desastres naturales.</p>
       </li>
       <li class="home__step">
         <ThirdStep />
-        <p>Imprimí tu álbum de fotos junto a los códigos QR relacionados.</p>
+        <p>Imprime tu álbum de fotos junto a los códigos QR relacionados.</p>
       </li>
     </ol>
     <div class="home__login">
-      <h4>¡Comenza ya!</h4>
+      <h4>¡Comienza ya!</h4>
         <CButton variant="red squared" @click="loginWithGoogle">
           <template #left-icon>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z" fill-rule="evenodd" clip-rule="evenodd"/></svg>
